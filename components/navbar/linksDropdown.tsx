47fb52b2d4f68673d1b6dedc3a073960e1674ac3
Abdullah Hussaini <h.abdullah1394@gmail.com>
2026-02-03 23:16:23 +0430

    Update linksDropdown.tsx

--- components/navbar/linksDropdown.tsx
+++ components/navbar/linksDropdown.tsx
@@ -1,4 +1,31 @@
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+    DropdownMenuSeparator
+} from "@/components/ui/dropdown-menu";
+import { LuAlignLeft } from "react-icons/lu";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { links } from '@/utils/links';
+
 function LinksDropdown() {
-    return <div>links dropdown</div>
+    return <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+            <Button variant="outline" className="flex gap-4 max-w-[100px]">
+                <LuAlignLeft className="w-6 h-6" />
+                <span className="sr-only">Open links menu</span>
+            </Button>
+            <DropdownMenuContent className="w-40" align="start" sideOffset={10}>
+                {links.map((link) => (
+                    <DropdownMenuItem key={link.id}>
+                        <Link href={link.url}>{link.label}</Link>
+                    </DropdownMenuItem>
+                ))}
+            </DropdownMenuContent>
+        </DropdownMenuTrigger>
+    </DropdownMenu>
 }
+
 export default LinksDropdown;
