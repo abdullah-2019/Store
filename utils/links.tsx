35965656b38ba42aa048af8090f044dcdfbf5a7d
Abdullah Hussaini <h.abdullah1394@gmail.com>
2026-02-03 23:00:55 +0430

    Create links.tsx

---
+++ utils/links.tsx
@@ -0,0 +1,12 @@
+type NavLinks = {
+    href: string;
+    label: string;
+};
+export const navLinks: NavLinks[] = [
+    { href: '/', label: 'Home' },
+    { href: '/about', label: 'About' },
+    { href: '/products', label: 'Products' },
+    { href: '/favorites', label: 'Favorites' },
+    { href: '/cart', label: 'Cart' },
+    { href: '/orders', label: 'Orders' },
+];
