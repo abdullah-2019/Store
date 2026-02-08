256441b0d61baa5662a087c00ebe6b3d90e8afc7
Abdullah Hussaini <h.abdullah1394@gmail.com>
2026-02-08 21:18:41 +0430

    Update Hero.tsx
    hero is done

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import Link from 'next/link';
+import { Button } from '../ui/button';
+import HeroCarousel from './HeroCarousel';
 
 export default function Hero() {
-  return (
-    <div>Hero</div>
-  )
+  return <section className="grid grid-cols-1 lg:grid-cols-2 gap-24 items-center">
+    <div>
+      <h1 className='max-w-2xl font-bold text-4xl tracking-light sm:text-6xl'>
+        We are changing the way people shop
+      </h1>
+      <p className='mt-8 max-w-xl text-lg leading-8 text-muted-foreground'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, at hic. Omnis officiis vero quae tempore ipsam dolores nihil iusto?
+      </p>
+      <Button className='mt-10' asChild size='lg'>
+        <Link href="/products">
+          Our Products
+        </Link>
+      </Button>
+    </div>
+    <HeroCarousel />
+  </section>;
 }
