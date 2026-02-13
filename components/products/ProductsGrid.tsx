7592f3cbb35e378a88865e892e78737afb4237c9
Abdullah Hussaini <h.abdullah1394@gmail.com>
2026-02-13 22:22:03 +0430

    Update ProductsGrid.tsx

--- components/products/ProductsGrid.tsx
+++ components/products/ProductsGrid.tsx
@@ -7,7 +7,7 @@ import FavoriteToggleButton from "./FavoriteToggleButton";
 
 function ProductsGrid({ products }: { products: Product[] }) {
   return (
-    <div className="pt-12 grid gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+    <div className="grid gap-3 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {products.map((product) => {
         const { name, price, image } = product;
         const productId = product.id;
@@ -15,8 +15,9 @@ function ProductsGrid({ products }: { products: Product[] }) {
         return <article key={productId} className="group relative">
           <Link href={`products/${productId}`}>
             <Card className="transform group-hover:shadow-xl transition-shadow duration-500">
-              <CardContent className="p-4">
-                <div className="relative h-64 md:h-48 rounded overflow-hidden">
+              <CardContent className="p-1">
+                {/* Added pt-1 to match the p-1 padding */}
+                <div className="relative h-56 md:h-40 rounded overflow-hidden bg-gray-100 pt-1 px-1">
                   <Image
                     src={image}
                     alt={name}
@@ -26,13 +27,13 @@ function ProductsGrid({ products }: { products: Product[] }) {
                     className="rounded w-full object-cover transform group-hover:scale-110 transition-transform duration-500" />
                 </div>
                 <div className="mt-4 text-center">
-                  <h2 className="text-lg capitalize">{name}</h2>
-                  <p className="text-muted-foreground mt-2">{dollarsAmount}</p>
+                  <h2 className="text-base capitalize">{name}</h2>
+                  <p className="text-muted-foreground mt-1">{dollarsAmount}</p>
                 </div>
               </CardContent>
             </Card>
           </Link>
-          <div className="absolute top-7 right-7 x-5">
+          <div className="absolute top-3 right-3 z-5">
             <FavoriteToggleButton productId={productId} />
           </div>
         </article>
