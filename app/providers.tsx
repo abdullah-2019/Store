49957ff604997d6d4d3644f5db077f027d4f549f
Abdullah Hussaini <h.abdullah1394@gmail.com>
2026-02-20 20:45:41 +0430

    Update providers.tsx
    Toaster added

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,9 +1,11 @@
 'use client'
 import { ThemeProvider } from "./theme-provider";
+import { Toaster } from "@/components/ui/sonner";
 
 export default function providers({ children }: { children: React.ReactNode }) {
     return (
         <>
+            <Toaster />
             <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
                 {children}
             </ThemeProvider>
