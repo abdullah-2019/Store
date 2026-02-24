87fb7ade3c0460b77bb685980d32be0ee1a9dbe4
Abdullah Hussaini <h.abdullah1394@gmail.com>
2026-02-24 20:44:22 +0430

    Update userIcon.tsx
    user icon fixed

--- components/navbar/userIcon.tsx
+++ components/navbar/userIcon.tsx
@@ -1,4 +1,5 @@
-import { LuUser2 } from "react-icons/lu";
+import { LuUser } from "react-icons/lu"; 
+
 import { currentUser, auth } from "@clerk/nextjs/server";
 
 
@@ -8,7 +9,7 @@ async function UserIcon() {
     if (profileImage) {
         return <img src={profileImage} className="w-6 h-6 rounded-full object-cover" />
     }
-    return <LuUser2 className="w-6 h-6 bg-primary rounded-full text-white" />
+    return <LuUser className="w-6 h-6 bg-primary rounded-full text-white" />
 }
 
 export default UserIcon;
