6dd6e312204af8c3836c9be4d4fb9786ffef55ca
Abdullah Hussaini <h.abdullah1394@gmail.com>
2026-02-24 21:09:42 +0430

    Update userIcon.tsx

--- components/navbar/userIcon.tsx
+++ components/navbar/userIcon.tsx
@@ -9,7 +9,7 @@ async function UserIcon() {
     if (profileImage) {
         return <img src={profileImage} className="w-6 h-6 rounded-full object-cover" />
     }
-    return <LuUser className="w-6 h-6 bg-primary rounded-full text-white" />
+    return <LuUser2 className="w-6 h-6 bg-primary rounded-full text-white" />
 }
 
 export default UserIcon;
